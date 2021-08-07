2f4280c0d2201426bb349a4dc0a9249006a068bd
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2021-08-07 13:03:31 -0600

    Se añadio Parametros
    Se crearon las funciones de las operaciones de los parametros

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import datetime
-
-date = datetime.datetime.now()
+from parametros import Parametros
 from tkinter.filedialog import askopenfilename
+import tkinter as tk
 
+root = tk.Tk() 
+root.iconify()
 
 def leer(ruta):
     archivo = open(ruta, "r")
@@ -10,7 +11,7 @@ def leer(ruta):
     archivo.close()
     return contenido
 
-def analizar(texto):
+def analizar(texto, type):
     
     nombre_curso = ""
     estudiantes = ""
@@ -27,7 +28,7 @@ def analizar(texto):
             fin_curso = True
         elif c1 == "{":
             inicio_estudiantes = True
-        elif c1 != "}" and c1 != "{" and inicio_estudiantes == True:
+        elif c1 != "}" and inicio_estudiantes == True:
             estudiantes += c1
         elif c1 == "}":
             inicio_parametros = True
@@ -48,22 +49,45 @@ def analizar(texto):
             elif c == ">":
                 break  
         lista_estudiantes.append(nuevo_valor)
- 
-    #print(lista_estudiantes)
     
-    diccionario_estudiantes = {}
+    lista_final = []
     for estudiante in lista_estudiantes:
         valor = estudiante.split(";")
-        diccionario_estudiantes[valor[0]] = int(valor[1])
+        t = (valor[0],int(valor[1]))
+        lista_final.append(t)
+    
+    
+    
         
-    print(nombre_curso)
-    print(diccionario_estudiantes)  
-    print(lista_parametros)
-            
+while(True):
+    print("")
+    print("--CONTROL ACADEMICO FACULTAD DE INGENIERIA--")
+    print("|| Seleccione accion a realizar:          ||")
+    print("|| 1. Cargar Archivo                      ||")
+    print("|| 2. Mostrar Reporte en Consola          ||")
+    print("|| 3. Exportar Reporte                    ||")
+    print("|| 4. Salir                               ||")
+    print("--------------------------------------------")
+    res = input()
+
+    archivo_cargado = False
+    text = ""
+
+    if res == "1":
+        filename = askopenfilename()
+        text = leer(filename)
+        print(text)
+    elif res == "2":
+        print("Console")
+    elif res == "4":
+        quit()
+    else:
+        print("not here")
     
 
-filename = askopenfilename()
-print(filename)
-text = leer(filename)
-#print(analizar(text))
-analizar(text)
+# filename = askopenfilename()
+# print(filename)
+# text = leer(filename)
+# print(text)
+# #print(analizar(text))
+# analizar(text)
---
+++ parametros.py
@@ -0,0 +1,78 @@
+
+class Parametros:
+     
+    def ascendente(self, lista):
+        self.lista = lista
+        for x in range(len(self.lista)-1, -1, -1):
+            swapped = False
+            for i in range(x):
+                if self.lista[i][1] > self.lista[i+1][1]:
+                    self.lista[i], self.lista[i+1] = self.lista[i+1], self.lista[i]
+                    swapped = True
+            if not swapped:
+                break
+        return self.lista
+    
+    def descendente(self, lista):
+        self.lista = lista
+        for x in range(len(self.lista)-1, -1, -1):
+            swapped = False
+            for i in range(x):
+                if self.lista[i][1] < self.lista[i+1][1]:
+                    self.lista[i], self.lista[i+1] = self.lista[i+1], self.lista[i]
+                    swapped = True
+            if not swapped:
+                break
+        return self.lista
+    
+    def promedio(self, lista):
+        acumulado = 0
+        cont = 0
+        for i in lista:
+            acumulado += lista[cont][1]
+            cont += 1
+        prom = acumulado/len(lista)
+        return prom
+    
+    def minimo(self,lista):
+        lista = lista
+        for x in range(len(lista)-1, -1, -1):
+            swapped = False
+            for i in range(x):
+                if lista[i][1] > lista[i+1][1]:
+                    lista[i], lista[i+1] = lista[i+1], lista[i]
+                    swapped = True
+            if not swapped:
+                break
+        return lista[0]
+    
+    def maximo(self,lista):
+        lista = lista
+        for x in range(len(lista)-1, -1, -1):
+            swapped = False
+            for i in range(x):
+                if lista[i][1] < lista[i+1][1]:
+                    lista[i], lista[i+1] = lista[i+1], lista[i]
+                    swapped = True
+            if not swapped:
+                break
+        return lista[0]
+    
+    def aprobado(self,lista):
+        acumulado = 0
+        cont = 0
+        for i in lista:
+            if lista[cont][1] >= 61:
+                acumulado+=1
+            cont += 1
+        return acumulado  
+    
+    def reprobado(self,lista):
+        acumulado = 0
+        cont = 0
+        for i in lista:
+            if lista[cont][1] < 61:
+                acumulado+=1
+            cont += 1
+        return acumulado  
+
